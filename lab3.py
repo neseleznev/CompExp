8e0fdbd5e9f10fb31fc2fdd34b7045842d1e56bb
Roman <Roman230394@yandex.ru>
2015-11-10 20:39:09 +0500

    оба метода

--- lab3.py
+++ lab3.py
@@ -1,3 +1,5 @@
+import copy
+
 N = 4
 M = 20 - N
 A = 0.1 * M + 0.01 * N
@@ -19,18 +21,10 @@ ACCURACY_AFTER_DOT = 0
 def round(value, accuracy):
     return int((value * (10 ** accuracy))) / (10 ** accuracy)
 
-def num_of_non_zero_elements(list):
-    num = 0
-    for i in range(len(list)):
-        if abs(list[i]) > 0:
-            num += 1
-            
-    return num
-
 def get_indexes_of_no_zero_items(list):
     indexes = []
     for i in range(len(list)):
-        if abs(list[i]) > 0:
+        if abs(round(list[i], ACCURACY_AFTER_DOT - 1)) > 0:
             indexes.append(i)
     return indexes
     
@@ -71,16 +65,13 @@ def calculate_cell_by_gauss(i, j, matrix_a, matrix_b, matrix_c):
     for k in range(i + 1, len(matrix_b[1])):
         sum += round(matrix_b[i][k] * matrix_c[k][j], ACCURACY_AFTER_DOT)
     
-    #print(i, j)
-    #print_matrix(matrix_b)
-    #print()
     if (j > i): # this element is in C
         matrix_c[i][j] = round((matrix_a[i][j] - sum)/matrix_b[i][i], ACCURACY_AFTER_DOT)
 	
     else: # element in B			
         matrix_b[i][j] = round((matrix_a[i][j] - sum)/matrix_c[i][i], ACCURACY_AFTER_DOT)
-		        
-               
+
+
 def calculate_matrix_B_nad_C_by_gauss(matrix_a):
     matrix_b = [
     [0, 0, 0],
@@ -97,12 +88,15 @@ def calculate_matrix_B_nad_C_by_gauss(matrix_a):
     height = len(matrix_a)
     weedth = len(matrix_a[0])
 
-    for k in range(0, weedth):
-        for i in range(k, height):
-            calculate_cell_by_gauss(i, k, matrix_a, matrix_b, matrix_c)
-        for j in range(k + 1, weedth):
-            calculate_cell_by_gauss(k, j, matrix_a, matrix_b, matrix_c)
-            
+    try:
+        for k in range(0, weedth):
+            for i in range(k, height):
+                calculate_cell_by_gauss(i, k, matrix_a, matrix_b, matrix_c)
+            for j in range(k + 1, weedth):
+                calculate_cell_by_gauss(k, j, matrix_a, matrix_b, matrix_c)
+    except ZeroDivisionError:
+        raise Exception("Method can not find solution")
+        
     return matrix_b, matrix_c
     
 def solve_up_triangle_system(matrix, b_column):
@@ -119,10 +113,51 @@ def solve_up_triangle_system(matrix, b_column):
         for k in range(i+1, len(b_column)):
             sum += round(matrix[i][k] * x_column[k][0], ACCURACY_AFTER_DOT)
         
-        x_column[i][0] = round(b_column[i][0]/matrix[i][i] - sum, ACCURACY_AFTER_DOT)
+        x_column[i][0] = round((b_column[i][0] - sum)/matrix[i][i], ACCURACY_AFTER_DOT)
         
     return x_column
         
+    
+def solve_any_transformed_system(set_of_equations):
+    x_column = []
+    for i in range(len(set_of_equations[0]) - 1):
+        x_column.append([0])
+        
+    
+    computed_elements_of_x = []
+    num_of_computed_x_on_last_iteartion = 0
+    no_zero_indexes_on_last_iteration = []
+    
+    while (len(computed_elements_of_x) != len(x_column)):
+        
+        for i in range(len(set_of_equations)):
+            indexes_of_no_zero_items = get_indexes_of_no_zero_items(set_of_equations[i][:-1])
+            
+            new_no_zero_elements = list(set(indexes_of_no_zero_items) - set(no_zero_indexes_on_last_iteration))
+            
+            if (len(new_no_zero_elements) != 1):
+                continue
+            
+            j = new_no_zero_elements[0]
+
+            sum = 0
+            for k in range(0, j):
+                sum += round(set_of_equations[i][k] * x_column[k][0], ACCURACY_AFTER_DOT)
+            
+            for k in range(j + 1, len(set_of_equations[0]) - 1):
+                sum += round(set_of_equations[i][k] * x_column[k][0], ACCURACY_AFTER_DOT)
+                
+            x_column[j][0] = round((set_of_equations[i][-1] - sum)/set_of_equations[i][j], ACCURACY_AFTER_DOT)
+            
+            computed_elements_of_x.append(j)
+            num_of_computed_x_on_last_iteartion += 1
+            no_zero_indexes_on_last_iteration = new_no_zero_elements
+
+            break
+        
+        if (num_of_computed_x_on_last_iteartion == len(computed_elements_of_x) - 1):
+            raise Exception("Method can not find solution!!!!")
+    return x_column
     
 def compact_gauss_scheme(set_of_equations):
     matrix_b, matrix_c = calculate_matrix_B_nad_C_by_gauss(set_of_equations)
@@ -136,17 +171,17 @@ def compact_gauss_scheme(set_of_equations):
     
     x_by_method = solve_up_triangle_system(matrix_c, y_column)
 	
-    #print("Matrix B:")
-    #print_matrix(matrix_b)
-    #print()
-    #
-    #print("Matrix C:")
-    #print_matrix(matrix_c)
-    #print()
-    #
-    #print("column y:")
-    #print_matrix(y_column)
-    #print()
+    print("Matrix B:")
+    print_matrix(matrix_b)
+    print()
+    
+    print("Matrix C:")
+    print_matrix(matrix_c)
+    print()
+    
+    print("column y:")
+    print_matrix(y_column)
+    print()
     
     return x_by_method
 
@@ -169,58 +204,40 @@ def swap_matrix_lines(matrix, string_num_1, string_num_2):
     matrix[string_num_1] = matrix[string_num_2]
     matrix[string_num_2] = temp
    
-def main_item_gauss(set_of_equations):
-        for start_string_index in range(len(set_of_equations)):
+   
+def do_forward_step_of_gauss_by_main_element(matrix):
+    set_of_equations = copy.deepcopy(matrix)
+    
+    for start_string_index in range(len(set_of_equations)):
             matrix_a = [line[:-1] for line in set_of_equations]
             
             main_i, main_j = find_max_cell(matrix_a, start_string_index)
             swap_matrix_lines(set_of_equations, start_string_index, main_i)
             
             for i in range(start_string_index + 1, len(set_of_equations)):
-                multiplicator = -set_of_equations[i][main_j] / set_of_equations[start_string_index][main_j]
-                
+                multiplicator = round(-set_of_equations[i][main_j] / set_of_equations[start_string_index][main_j], ACCURACY_AFTER_DOT)
+
                 for j in range(len(set_of_equations[i])):
-                    set_of_equations[i][j] += set_of_equations[start_string_index][j] * multiplicator
+                    set_of_equations[i][j] = round(set_of_equations[i][j] + set_of_equations[start_string_index][j] * multiplicator, ACCURACY_AFTER_DOT)
+ 
+    return set_of_equations
+   
+def main_item_gauss(matrix):
+        set_of_equations = do_forward_step_of_gauss_by_main_element(matrix)
         
-        #set_of_equations = sorted(set_of_equations, key = lambda row: num_of_non_zero_elements(row))
-        
+        print("Set of equations after forward step of method:")
         print_matrix(set_of_equations)
+        print()
         
-        x_column = []
-        for i in range(len(set_of_equations[0]) - 1):
-            x_column.append([0])
-        
-        computed_elements_of_x = []
-        for i in range(len(set_of_equations)):
-            indexes_of_no_zero_items = get_indexes_of_no_zero_items(set_of_equations[i][:-1])
-            
-            print(indexes_of_no_zero_items)
-            j = list(set(indexes_of_no_zero_items) - set(computed_elements_of_x))[0]
-            
-            sum = 0
-            for k in range(0, j):
-                sum += set_of_equations[i][k] * x_column[k][0]
-            
-            for k in range(j + 1, len(set_of_equations) - 1):
-                sum += set_of_equations[i][k] * x_column[k][0]
-                
-            x_column[j][0] = (set_of_equations[i][-1]/set_of_equations[i][j] - sum)
-            
-            computed_elements_of_x.append(j)
-        
-        print_matrix(x_column)
+        x_column = solve_any_transformed_system(set_of_equations)
+        return x_column
         
         
+ 
     
 
     
-def do_first_part_of_lab():
-    print("real answer(X*):")
-    print_matrix(x_star)
-    print()
-
-
-
+def do_method_investigation(method):
     for accuracy in [2, 4, 6]:
         global ACCURACY_AFTER_DOT
         ACCURACY_AFTER_DOT = accuracy
@@ -232,25 +249,37 @@ def do_first_part_of_lab():
             for j in range(len(set_of_equations[i])):
                 copy_of_system[i].append(round(set_of_equations[i][j], ACCURACY_AFTER_DOT))
         
-        print("accuracy: ", accuracy)
+        print("\naccuracy: ", accuracy)
         
         try:
-            x_by_method = compact_gauss_scheme(copy_of_system)
-        except ZeroDivisionError:
-            print("accuracy {0} is not enough\n\n".format(accuracy))
+            x_by_method = method(copy_of_system)
+        except Exception as e:
+            print(e)
             continue
         
         print("answer:")
         print_matrix(x_by_method)
         print()
+        
 	
-#do_first_part_of_lab()	
 
-main_item_gauss(set_of_equations)
-	
-	
-	
-	
+    
+def main():
+    print("real answer(X*):")
+    print_matrix(x_star)
+    print("===============================================================")
+    print()
+    
+    print("Compact Gauss Scheme: ")
+    do_method_investigation(compact_gauss_scheme)	
+    print("===============================================================")
+    
+    print("Gauss Scheme with main element: ")
+    do_method_investigation(main_item_gauss)	
+
+main()
+#main_item_gauss(set_of_equations)
+
 
 
 
